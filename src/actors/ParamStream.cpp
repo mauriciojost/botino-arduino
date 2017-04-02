b6cd1988164001e0c33712698b0b808c24564d51
Mauricio Jost <mauricio.jost@gmail.com>
2017-04-02 23:07:41 +0200

    Simplify ParamStream

--- src/actors/ParamStream.cpp
+++ src/actors/ParamStream.cpp
@@ -7,63 +7,16 @@
 
 ParamStream::ParamStream() {
   commandsAvailable = 0;
-  nroBytesReceived = 0;
 }
 
 size_t ParamStream::write(uint8_t b) {
-
   append(b);
-
-  if (b == '&') {
-    if (nroBytesReceived != 0) {
-      Buffer<MAX_VALUE_STR_LENGTH> newValue;
-      // Parse the potential command: c<configurable>p<property>=<value>
-      char* beginStr = bytesReceived.getBuffer();
-      char* indexStr = strtok(beginStr, "=&");
-      char* newValueStr = strtok(NULL, "=&");
-      char* confStr = strtok(indexStr, "cp");
-      char* propStr = strtok(NULL, "cp");
-
-      if (confStr != NULL && propStr != NULL && newValueStr != NULL) {
-        int confIndex = atoi(confStr);
-        int propIndex = atoi(propStr);
-        newValue.load(newValueStr);
-        addCommand(confIndex, propIndex, &newValue);
-
-        log(CLASS, Info, "Command");
-        log(CLASS, Info, "- Conf: ", confIndex);
-        log(CLASS, Info, "- Prop: ", propIndex);
-        log(CLASS, Info, "- NewV: ", newValue.getBuffer());
-
-      } else {
-
-        log(CLASS, Warn, "Discarded command");
-        log(CLASS, Warn, "- Conf: ", confStr);
-        log(CLASS, Warn, "- Prop: ", propStr);
-        log(CLASS, Warn, "- NewV: ", newValueStr);
-
-      }
-    }
-    nroBytesReceived = 0;
-  }
-
   return 1;
 }
 
 void ParamStream::append(uint8_t b) {
   log(CLASS, Debug, "Byte: ", (int)b);
-  nroBytesReceived = (nroBytesReceived + 1) % (MAX_VALUE_STR_LENGTH + 1);
-  bytesReceived.getBuffer()[nroBytesReceived - 1] = b;
-}
-
-void ParamStream::addCommand(int confIndex, int propIndex, const Value* newValue) {
-  if (commandsAvailable < MAX_NRO_COMMANDS) {
-    commands[commandsAvailable].confIndex = confIndex;
-    commands[commandsAvailable].propIndex = propIndex;
-    commands[commandsAvailable].newValue.load(newValue);
-    commandsAvailable++;
-  }
-
+  bytesReceived.append(b);
 }
 
 int ParamStream::available() {
@@ -81,12 +34,4 @@ int ParamStream::peek() {
 void ParamStream::flush() {
   commandsAvailable = 0;
   bytesReceived.clear();
-}
-
-int ParamStream::getNroCommandsAvailable() {
-  return commandsAvailable;
-}
-
-Command* ParamStream::getCommands() {
-  return commands;
 }
--- src/actors/ParamStream.h
+++ src/actors/ParamStream.h
@@ -7,31 +7,21 @@
 #include <Stream.h>
 #endif // UNIT_TEST
 
-#define MAX_NRO_COMMANDS 4
-
-struct Command {
-  int confIndex;
-  int propIndex;
-  Buffer<MAX_VALUE_STR_LENGTH> newValue;
-};
-
 #ifndef UNIT_TEST
+
 class ParamStream : public Stream {
+
 #else // UNIT_TEST
+
 #define uint8_t unsigned char
 #define size_t int
 class ParamStream {
+
 #endif // UNIT_TEST
 
 private:
-  int commandsAvailable;
-  Command commands[MAX_NRO_COMMANDS];
-  int nroBytesReceived;
-  Buffer<MAX_VALUE_STR_LENGTH> bytesReceived;
-
-  void addCommand(int confIndex, int propIndex, const Value* newValue);
+  Buffer<MAX_JSON_STR_LENGTH> bytesReceived;
   void append(uint8_t b);
-
 public:
   ParamStream();
   size_t write(uint8_t);
@@ -39,9 +29,6 @@ public:
   int read();
   int peek();
   void flush();
-
-  Command* getCommands();
-  int getNroCommandsAvailable();
 
 };
 
