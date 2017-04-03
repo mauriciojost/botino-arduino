024b3064d7c0e8331507caa9e59ec0f2b8e0c4ee
Mauricio Jost <mauricio.jost@gmail.com>
2017-04-03 22:42:22 +0200

    Update dweetio-api.bash

--- misc/dweetio-api.bash
+++ misc/dweetio-api.bash
@@ -3,4 +3,8 @@
 set -e
 set -x 
 
-curl -H 'Content-Type:application/json' -X POST https://dweet.io:443/dweet/for/dev0 -D '{"hola":"campeon2"}'
+export PRETTY='python -m json.tool'
+
+curl -s -X POST 'http://dweet.io/dweet/for/dev0-target?led1.on=0' | $PRETTY
+
+curl -s -X GET 'http://dweet.io/get/latest/dweet/for/dev0' | $PRETTY
