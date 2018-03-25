bbbad2c007f983ad9b88e8e03f177d9fbe89f7c5
Mauricio Jost <mauricio.jost@gmail.com>
2018-03-25 19:30:57 +0200

    Enable servo pin

--- src/Main.cpp
+++ src/Main.cpp
@@ -97,9 +97,9 @@ void servoControl(int pos) {
 
 void setupPins() {
   log(CLASS_MAIN, Debug, "Setup pins");
-  //pinMode(LED0_PIN, OUTPUT);
+  //pinMode(LED0_PIN, OUTPUT); // breaks serial
   pinMode(LED1_PIN, OUTPUT); // will break deep sleep mode
-  //pinMode(SERVO0_PIN, OUTPUT);
+  pinMode(SERVO0_PIN, OUTPUT);
 }
 
 void setup() {
