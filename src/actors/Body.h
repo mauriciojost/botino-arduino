02c7a3533c5dba9e1dc08ad06f8dd04c0a45c1b3
Mauricio Jost <mauricio.jost@gmail.com>
2018-04-24 23:39:16 +0200

    Add sequence or positions for Body

--- src/actors/Body.h
+++ src/actors/Body.h
@@ -9,18 +9,19 @@
 
 #define CLASS_BODY "BO"
 #define LCD_LINE_LENGTH 32
+#define SEQUENCE_STR_LENGTH (2 * 8)
+
+#define SEQVAL(a, b) (int)(((int)(a)) * 256 + (b))
 
 enum BodyConfigState {
-  BodyConfigMood = 0,      // if the led is on
-  BodyConfigMsg,           // message
+  BodyConfigMsg = 0,         // message
+  BodyConfigSeq0,           // sequence
   BodyConfigTime,           // time of acting
   BodyConfigCron,           // cron of acting
   BodyConfigStateDelimiter // delimiter of the configuration states
 };
 
 enum ArmState { ArmUp = 0, ArmMiddle, ArmDown, ArmDelimiter };
-
-enum Mood { Sleepy = 0, Normal, Sad, Happy, BodyStateDelimiter };
 
 class Body : public Actor {
 
@@ -34,7 +35,7 @@ private:
   void (*arms)(ArmState left, ArmState right);
   void (*messageFunc)(int line, const char *msg);
   Buffer<LCD_LINE_LENGTH> *msg;
-  Mood mood;
+  Buffer<SEQUENCE_STR_LENGTH> *sequence0;
   long time;
   long cron;
 
@@ -42,55 +43,43 @@ private:
     return smilyFace != NULL && sadFace != NULL && normalFace != NULL && sleepyFace != NULL && arms != NULL && messageFunc != NULL;
   }
 
-  void actMood() {
-    switch (mood) {
-      case Happy:
-        log(CLASS_BODY, Debug, "Happy");
-        arms(ArmUp, ArmUp);
-        messageFunc(0, msg->getBuffer());
-        smilyFace();
-        messageFunc(0, msg->getBuffer());
-        smilyFace();
-        arms(ArmUp, ArmUp);
-        arms(ArmDown, ArmDown);
-        arms(ArmUp, ArmUp);
-        arms(ArmDown, ArmDown);
-        arms(ArmUp, ArmUp);
-        arms(ArmDown, ArmDown);
+  void actSub(char c1, char c2) {
+    switch (SEQVAL(c1, c2)) {
+    	// faces
+      case SEQVAL('f', 's'):
         smilyFace();
         break;
-      case Sad:
-        log(CLASS_BODY, Debug, "Sad");
-        arms(ArmUp, ArmUp);
-        messageFunc(0, msg->getBuffer());
-        sadFace();
-        messageFunc(0, msg->getBuffer());
-        sadFace();
-        arms(ArmUp, ArmUp);
-        arms(ArmMiddle, ArmMiddle);
-        arms(ArmDown, ArmDown);
-        arms(ArmMiddle, ArmMiddle);
-        arms(ArmDown, ArmDown);
+      case SEQVAL('f', 'S'):
         sadFace();
         break;
-      case Normal:
-        log(CLASS_BODY, Debug, "Normal");
-        arms(ArmMiddle, ArmMiddle);
-        messageFunc(0, msg->getBuffer());
+      case SEQVAL('f', 'n'):
         normalFace();
-        messageFunc(0, msg->getBuffer());
-        normalFace();
-        arms(ArmDown, ArmDown);
         break;
-      case Sleepy:
-        log(CLASS_BODY, Debug, "Sleepy");
-        messageFunc(0, msg->getBuffer());
-        sleepyFace();
-        messageFunc(0, msg->getBuffer());
-        sleepyFace();
-        arms(ArmDown, ArmDown);
+      case SEQVAL('f', 'l'):
         sleepyFace();
         break;
+      // arms
+      case SEQVAL('a', 'u'):
+        arms(ArmUp, ArmUp);
+        break;
+      case SEQVAL('a', 'r'):
+        arms(ArmDown, ArmUp);
+        break;
+      case SEQVAL('a', 'l'):
+        arms(ArmUp, ArmDown);
+        break;
+      case SEQVAL('a', 'd'):
+        arms(ArmDown, ArmDown);
+        break;
+      // messages
+      case SEQVAL('m', '1'):
+        messageFunc(0, msg->getBuffer());
+        break;
+      // misc
+      case SEQVAL('w', 'a'):
+        delay(1000);
+        break;
+      // default
       default:
         break;
     }
@@ -105,10 +94,10 @@ public:
     sleepyFace = NULL;
     arms = NULL;
     messageFunc = NULL;
-    mood = Sleepy;
     time = 0L;
     cron = 0L;
-    msg = new Buffer<LCD_LINE_LENGTH>("");
+    msg = new Buffer<LCD_LINE_LENGTH>("msg");
+    sequence0 = new Buffer<SEQUENCE_STR_LENGTH>("m1auadfsfn");
   }
 
   const char *getName() {
@@ -139,16 +128,20 @@ public:
       return;
     }
     if (freqConf.matches()) {
-      actMood();
+      const char* s = sequence0->getBuffer();
+    	for (int i = 0; i < strlen(s); i = i + 2) {
+        actSub(s[i], s[i + 1]);
+    	}
+
     }
   }
 
   const char *getPropName(int propIndex) {
     switch (propIndex) {
-      case (BodyConfigMood):
-        return "mo";
       case (BodyConfigMsg):
         return "ms";
+      case (BodyConfigSeq0):
+        return "s0";
       case (BodyConfigTime):
         return "ti";
       case (BodyConfigCron):
@@ -160,11 +153,11 @@ public:
 
   void setProp(int propIndex, SetMode setMode, const Value *targetValue, Value *actualValue) {
     switch (propIndex) {
-      case (BodyConfigMood):
-        setPropInteger(setMode, targetValue, actualValue, (int *)&mood);
-        break;
       case (BodyConfigMsg):
         setPropValue(setMode, targetValue, actualValue, msg);
+        break;
+      case (BodyConfigSeq0):
+        setPropValue(setMode, targetValue, actualValue, sequence0);
         break;
       case (BodyConfigTime):
         setPropLong(setMode, targetValue, actualValue, (long*)&time);
