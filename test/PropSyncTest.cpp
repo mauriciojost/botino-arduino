b0cb760bea0739a15bc0cc6aa9a6d6e317a40e03
Mauricio Jost <mauricio.jost@gmail.com>
2018-05-09 22:17:37 +0200

    Add PropSyncTest

--- test/PropSyncTest.cpp
+++ test/PropSyncTest.cpp
@@ -18,20 +18,19 @@ bool initWifi() {
 }
 
 int httpGet(const char* url, ParamStream* response) {
-	if (strcmp("", url) == 0) {
-		response->flush();
+
+	if (strcmp("http://dweet.io/get/latest/dweet/for/device1-clock-target", url) == 0) {
+		response->append("{\"with\":[{\"content\":{\"h\":3}}]}");
 	}
-  response->flush();
 	return 1;
 }
 
 int httpPost(const char* url, const char* body, ParamStream* response) {
-  response->flush();
 	return 1;
 }
 
 void test_propsync_syncs_properties() {
-	Clock clock("c");
+	Clock clock("clock");
 
 	Array<Actor*> actors(1);
 	actors.set(0, &clock);
@@ -45,9 +44,11 @@ void test_propsync_syncs_properties() {
   p.setHttpGet(httpGet);
   p.setHttpPost(httpPost);
 
-  //p.getFrequencyConfiguration()->setFrequency(OnceEvery1Second);
+  p.getFrequencyConfiguration()->setFrequency(OnceEvery1Second);
 
   p.act();
+
+  TEST_ASSERT_EQUAL(3, GET_HOURS(clock.currentTime()));
 
 }
 
