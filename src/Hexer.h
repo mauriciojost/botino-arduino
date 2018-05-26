6194c4b93a028a16218ddbca2889ad9efe00f434
Mauricio Jost <mauricio.jost@gmail.com>
2018-05-26 07:16:12 +0200

    Remove logs (for now) that seem to be causing trouble

--- src/Hexer.h
+++ src/Hexer.h
@@ -33,10 +33,10 @@ public:
   static void hexStrCpy(uint8_t* outputText, const char* inputHex, size_t l) {
   	if (l % 2 == 0) {
       int i;
-      log(CLASS_HEXER, Debug, "Parse hex: %s", inputHex);
+      //log(CLASS_HEXER, Debug, "Parse hex: %s", inputHex);
   		for(i = 0; i < l; i = i + 2) {
   			outputText[i / 2] = hexToValue(inputHex[i]) * 16 + hexToValue(inputHex[i + 1]);
-        log(CLASS_HEXER, Debug, "  %.2x <- %c %c", outputText[i / 2], inputHex[i], inputHex[i+1]);
+        //log(CLASS_HEXER, Debug, "  %.2x <- %c %c", outputText[i / 2], inputHex[i], inputHex[i+1]);
   		}
   		outputText[i / 2] = 0;
   	} else {
