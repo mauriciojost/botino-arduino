35dab1ae8b6f681dd9bd24f022066e2b0585cc5c
Mauricio Jost <mauricio.jost@gmail.com>
2018-07-13 07:50:11 +0200

    Post too

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,8 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
+HTTPClient httpClient;
+
 void setup() {
   delay(2 * 1000);
   Serial.begin(115200);
@@ -39,17 +41,26 @@ bool initWifi(const char *ssid, const char *pass, int retries) {
 
 void loop() {
   bool connected = initWifi("aphone", "apassword", 20);
-  HTTPClient httpClient;
-  const char* url = "http://dweet.io/get/latest/dweet/for/demo2-setupsync-target";
-  httpClient.begin(url);
+
+  const char* urlGet = "http://dweet.io/get/latest/dweet/for/kokaka";
+  httpClient.begin(urlGet);
   httpClient.addHeader("Content-Type", "application/json");
-  Serial.printf("> GET:..%s\n", url);
-  int errorCode = httpClient.GET();
-  //int errorCode2 = httpClient.POST(body);
-  Serial.printf("> GET:%d\n", errorCode);
-  int e = httpClient.writeToStream(&Serial);
-  Serial.printf("> GET(%d):%d %s\n", e, errorCode, httpClient.errorToString(errorCode).c_str());
+  Serial.printf("> GET:..%s\n", urlGet);
+  int errorCodeGet = httpClient.GET();
+  Serial.printf("> GET:%d\n", errorCodeGet);
+  int e1 = httpClient.writeToStream(&Serial);
+  Serial.printf("> GET(%d):%d %s\n", e1, errorCodeGet, httpClient.errorToString(errorCodeGet).c_str());
   httpClient.end();
+
+  const char* urlPost = "http://dweet.io/dweet/for/kokaka";
+  httpClient.begin(urlPost);
+  httpClient.addHeader("Content-Type", "application/json");
+  int errorCodePost = httpClient.POST("{\"a\": \"tot\"}");
+  Serial.printf("> POST:%d\n", errorCodePost);
+  int e2 = httpClient.writeToStream(&Serial);
+  Serial.printf("> POST(%d):%d %s\n", e2, errorCodePost, httpClient.errorToString(errorCodePost).c_str());
+  httpClient.end();
+
   delay(1000);
 
 }
