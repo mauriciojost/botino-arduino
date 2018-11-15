ea9ef1a7d09aeee253f66db55d6bef153e630e16
Mauricio Jost <mauricio.jost@gmail.com>
2018-11-15 11:25:19 +0100

    Put hwTest into a new file

--- src/MainESP8266.h
+++ src/MainESP8266.h
@@ -92,7 +92,7 @@ Adafruit_SSD1306 lcd(-1);
 
 void bitmapToLcd(uint8_t bitmap[]);
 void reactCommandCustom();
-void hwTest();
+#include "MainESP8266_hwtest.h" // defines hwTest()
 void buttonPressed();
 bool reactToButtonHeld(int cycles, bool onlyMsg);
 
@@ -637,93 +637,4 @@ void bitmapToLcd(uint8_t bitmap[]) {
 
 void reactCommandCustom() { // for the use via telnet
   m.command(Telnet.getLastCommand().c_str());
-}
-
-void hwTest() {
-
-  Buffer aux(32);
-  log(CLASS_MAIN, Debug, "USER INFO");
-  aux.fill("VER: %s", STRINGIFY(PROJ_VERSION));
-  m.getNotifier()->message(0, 2, aux.getBuffer());
-  log(CLASS_MAIN, Debug, aux.getBuffer());
-  delay(USER_DELAY_MS);
-  aux.fill("NAM: %s", DEVICE_NAME);
-  m.getNotifier()->message(0, 2, aux.getBuffer());
-  log(CLASS_MAIN, Debug, aux.getBuffer());
-  delay(USER_DELAY_MS);
-  aux.fill("ID : %d", ESP.getChipId());
-  m.getNotifier()->message(0, 2, aux.getBuffer());
-  log(CLASS_MAIN, Debug, aux.getBuffer());
-  delay(USER_DELAY_MS);
-  log(CLASS_MAIN, Debug, "HW test");
-
-#ifdef HARDWARE_TEST
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  ios('r', false);
-  ios('y', false);
-  ios('w', false);
-  ios('f', false);
-  lcdImg('l', NULL);
-
-  log(CLASS_MAIN, Debug, "..Face test");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  lcdImg('c', initImage);
-  log(CLASS_MAIN, Debug, "..Arms down");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  arms(0, 0, 100);
-  log(CLASS_MAIN, Debug, "..R. arm up");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  arms(0, 3, 100);
-  log(CLASS_MAIN, Debug, "..Left arm up");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  arms(3, 3, 100);
-  log(CLASS_MAIN, Debug, "..Arms down");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  arms(0, 0, 100);
-  log(CLASS_MAIN, Debug, "..Red led on");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  ios('r', true);
-  log(CLASS_MAIN, Debug, "..Red led off");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  ios('r', false);
-  log(CLASS_MAIN, Debug, "..Y. led on");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  ios('y', true);
-  log(CLASS_MAIN, Debug, "..Y. led off");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  ios('y', false);
-  log(CLASS_MAIN, Debug, "..W. led on");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  ios('w', true);
-  log(CLASS_MAIN, Debug, "..W. led off");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  ios('w', false);
-  log(CLASS_MAIN, Debug, "..Fan on");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  ios('f', true);
-  log(CLASS_MAIN, Debug, "..Fan off");
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-  ios('f', false);
-  log(CLASS_MAIN, Debug, "..Random %lu %lu %lu", random(10000), random(10000), random(10000));
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-
-  log(CLASS_MAIN, Debug, "..SPIFFS");
-  SPIFFS.begin();
-  File f = SPIFFS.open("/version.txt", "r");
-  if (!f) {
-    log(CLASS_MAIN, Warn, "File reading failed");
-  } else {
-    String s = f.readString();
-    log(CLASS_MAIN, Info, "File content: %s", s.c_str());
-  }
-  SPIFFS.end();
-  delay(HARDWARE_TEST_STEP_DELAY_MS);
-
-#endif
-
-  ios('r', false);
-  ios('y', false);
-  ios('w', false);
-  ios('f', false);
-  lcdImg('l', NULL);
 }
