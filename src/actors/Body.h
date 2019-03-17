abd3bf2eccd7dde74a82c9c3f141797bc1f3017e
Mauricio Jost <mauricio.jost@gmail.com>
2019-03-17 17:29:38 +0100

    Move Body documentation to a dedicated .md

--- src/actors/Body.h
+++ src/actors/Body.h
@@ -6,10 +6,10 @@
  *
  * Representative of the Botino physical robot.
  *
- * The robot can perform a few configurable routines at a given moment / frequency in time.
+ * The robot can perform a few configurable routines with a given timing.
  *
  * The most important concept is the routine, which is made of:
- * - a timing condition (can be a frequency expression or a specific time of the day)
+ * - a timing condition
  * - a move to perform once the timing condition matches (a sequence of poses)
  *
  * A move can contain many poses. A pose can be: both arms up, both arms down, a given face (sad, smily) or even some
@@ -18,110 +18,6 @@
  * Routines are configurable in both senses: timing and moves.
  *
  */
-
-/*
-
-POSES (X-char codes separated by separator)
---------------------
-
-ARMS POSES: move both arms to a given position each (left, then right) (A=fast, B=normal, C=slow)
-Codes:
-  A00. : Move left and right arms to respective position 0 and 0 (both down) at high speed
-  ...
-  A90. : Move left and right arms to respective position 9 and 0 (left arm up) at high speed
-  ...
-  A99. : Move left and right arms to respective position 9 and 9 (both up) at high speed
-
-  B99. : Move left and right arms to respective position 9 and 9 (both up) at normal speed
-
-  C99. : Move left and right arms to respective position 9 and 9 (both up) at low speed
-
-
-COMPOSED POSES: dances and other predefined moves usable as poses
-Codes:
-  Dn. : dance n
-  Du. : dance u
-  D\. : dance \
-  D/. : dance /
-
-  D0. : dance 0
-  D1. : dance 1
-  D2. : dance 2
-  D3. : dance 3
-
-
-FACE POSES: show a given image in the LCD
-Codes:
-  Fw. : Face White
-  Fb. : Face Black
-  Fa. : Face Angry
-  Fr. : Face cRazy
-  Fl. : Face cLear
-  Fs. : Face Smily
-  FS. : Face Sad
-  Fn. : Face Normal
-  Fz. : Face Zleepy
-  F_. : Face dimmed
-  F-. : Face bright
-  F0. : Face custom 0 (user provided)
-  F1. : Face custom 1 (user provided)
-  F2. : Face custom 2 (user provided)
-  F3. : Face custom 3 (user provided)
-
-
-IFTTT EVENTS: trigger an ifttt event (by index, given the configuration of the ifttt module)
-Codes:
-  Ix. : trigger event 'x'
-
-
-IFTTT EVENTS: trigger an ifttt event (by its name)
-Codes:
-  Iname. : trigger event 'name'
-
-
-IO POSES: turn on/off a given IO device, such as LEDS or the FAN (on = y, off = n)
-Codes:
-  L?.. : turn randomly all leds
-
-
-IO POSES: turn on/off a given IO device, such as LEDS or the FAN (on = y, off = n)
-Codes:
-  Lry. : turn on (y) the Red led
-  Lrn. : turn off (n) the Red led
-  Lrt. : toggle (t) the Red led
-  Lwy. : turn on (y) led White
-  Lyy. : turn on (y) led Yellow
-  Lyt. : toggle (t) led Yellow
-  L?.. : turn randomly all leds
-  Lfy. : turn on (y) Fan
-
-
-MESSAGE POSES: show a certain message in the LCD with a given font size
-Codes:
-  Mc4. : show message containing current time (with font size 4)
-  Mk3. : show message containing current date-time (with font size 3)
-  Mp1. : show random future reading (with font size 1)
-  Mq1. : show random quote (with font size 1)
-  M1HELLO. : show message HELLO with font size 1 (user provided)
-
-
-NOTIFICATION POSES: show a certain notification in the LCD (requires user's ACK before removal)
-Codes:
-  NHELLO. : show notification HELLO
-
-
-WAIT POSES: wait a given number of seconds
-Codes:
-  W1. : wait 1 second
-  ...
-  W9. : wait 9 seconds
-
-
-MISC POSES
-Codes:
-  Z. : turn all power consuming components off
-
-*/
 
 #include <utils/Hexer.h>
 #include <utils/Io.h>
---
+++ src/actors/Body.md
@@ -0,0 +1,122 @@
+# POSES
+
+## ARMS POSES
+
+Move both arms to a given position each (left, then right) (A=fast, B=normal, C=slow).
+
+Codes:
+  A00. : Move left and right arms to respective position 0 and 0 (both down) at high speed
+  ...
+  A90. : Move left and right arms to respective position 9 and 0 (left arm up) at high speed
+  ...
+  A99. : Move left and right arms to respective position 9 and 9 (both up) at high speed
+
+  B99. : Move left and right arms to respective position 9 and 9 (both up) at normal speed
+
+  C99. : Move left and right arms to respective position 9 and 9 (both up) at low speed
+
+
+## COMPOSED POSES
+
+Dances and other predefined moves usable as poses.
+
+Codes:
+  Dn. : dance n
+  Du. : dance u
+  D\. : dance \
+  D/. : dance /
+
+  D0. : dance 0
+  D1. : dance 1
+  D2. : dance 2
+  D3. : dance 3
+
+
+## FACE POSES
+
+Show a given image in the LCD.
+
+Codes:
+  Fw. : Face White
+  Fb. : Face Black
+  Fa. : Face Angry
+  Fr. : Face cRazy
+  Fl. : Face cLear
+  Fs. : Face Smily
+  FS. : Face Sad
+  Fn. : Face Normal
+  Fz. : Face Zleepy
+  F_. : Face dimmed
+  F-. : Face bright
+  F0. : Face custom 0 (user provided)
+  F1. : Face custom 1 (user provided)
+  F2. : Face custom 2 (user provided)
+  F3. : Face custom 3 (user provided)
+
+
+## IFTTT EVENTS
+
+Trigger an ifttt event by its name.
+
+Codes:
+  I<name>. : trigger event 'name'
+
+
+## IO POSES
+
+Turn on/off a given IO device, such as LEDS or the FAN (on = y, off = n).
+
+
+## IO POSES
+
+Turn on/off a given IO device, such as LEDS or the FAN (on = y, off = n).
+
+Codes:
+  Lry. : turn on (y) the Red led
+  Lrn. : turn off (n) the Red led
+  Lrt. : toggle (t) the Red led
+  Lwy. : turn on (y) led White
+  Lyy. : turn on (y) led Yellow
+  Lyt. : toggle (t) led Yellow
+  L*y. : turn all leds on
+  L*n. : turn all leds off
+  Lfy. : turn on (y) Fan
+  L?.  : turn randomly all leds
+
+
+
+## MESSAGE POSES
+
+Show a certain message in the LCD with a given font size.
+
+Codes:
+  Mc4. : show message containing current time (with font size 4)
+  Mk3. : show message containing current date-time (with font size 3)
+  Mp1. : show random future reading (with font size 1)
+  Mq1. : show random quote (with font size 1)
+  M1<msg>. : show message msg with font size 1 (user provided)
+
+
+## NOTIFICATION POSES
+
+Show a certain notification in the LCD (requires user's ACK before removal).
+
+Codes:
+  N<notif>. : show notification "notif"
+
+
+## WAIT POSES
+
+Wait a given number of seconds.
+
+Codes:
+  W1. : wait 1 second
+  ...
+  W9. : wait 9 seconds
+
+
+## MISC POSES
+
+Codes:
+  Z. : turn all power consuming components off
+
