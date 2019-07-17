48942afc7baa37005d22f8b5e569a8f090f062d0
Mauricio Jost <mauricio.jost@gmail.com>
2019-07-17 06:38:52 +0200

    Add almost all descriptions

--- src/Main.cpp
+++ src/Main.cpp
@@ -19,10 +19,99 @@ ModuleBotino *m;
 
 
 #define VERSION_DESCRIPTION_JSON "{"\
-	"\"version\":\"" STRINGIFY(PROJ_VERSION) "\","\
-	"\"json\":\"["\
-   "{\\\"patterns\\\": [\\\"^actor1.p1$\\\"], \\\"descriptions\\\": [\\\"Property 1\\\"], \\\"examples\\\": [\\\"1\\\", \\\"2\\\"]}"\
-	 "]\"}"
+  "\"version\":\"" STRINGIFY(PROJ_VERSION) "\","\
+  "\"json\":["\
+    "{"\
+      "\"patterns\": [\"^.*\\\\.freq$\"],"\
+      "\"descriptions\": [\"Frequency at which the actor will act and perform its duty. See <a href=\\\"https://bitbucket.org/mauriciojost/main4ino-arduino/src/master/README.md\\\" target=\\\"_blank\\\">here</a>.\"],"\
+      "\"examples\": ["\
+        "\"never     -> Never\","\
+        "\"@08h00   -> At 08h00 every day\","\
+        "\"~30m  -> Every 30 minutes\","\
+        "\"~10s  -> Every 10 seconds\","\
+        "\"[smTWtfs]~30mf6t18 -> Every 30 minutes from 06h until 18h on Tuesdays and Wednesdays\","\
+        "\"[SMtwtfs]@15h00 -> At 15h00 on Sundays and Mondays\","\
+        "\"oneoff    -> Once and go back to previous configuration\""\
+      "]"\
+    "},"\
+    "{"\
+      "\"patterns\": [\"^clocksync\\\\.zone$\"],"\
+      "\"descriptions\": [\"Zone to pick up the current date-time from. See <a href=\\\"https://timezonedb.com/\\\" target=\\\"_blank\\\">here</a>.\"],"\
+      "\"examples\": ["\
+        "\"Europe/Paris\","\
+        "\"America/Argentina\","\
+        "\"Asia/Shanghai\","\
+        "\"Africa/Freetown\""\
+      "]"\
+      "},"\
+      "{"\
+      "\"patterns\": [\"^notifier\\\\.n\\\\d{1,2}\"],"\
+      "\"descriptions\": [\"Notification item i-th.\"],"\
+      "\"examples\": [\"Drink water\"]"\
+    "},"\
+    "{"\
+      "\"patterns\": [\"^moves\\\\.mv\\\\d{1,2}\"],"\
+      "\"descriptions\": [\"Move pattern i-th: move name, : character, and command to execute.\"],"\
+      "\"examples\": ["\
+        "\"Arms up:move C99.\","\
+        "\"Update:update\","\
+        "\"Execute IFTTT event:move Ievent.\","\
+        "\"Acknowledge notification: ack\""\
+      "]"\
+    "},"\
+    "{"\
+      "\"patterns\": [\"^images\\\\.im\\\\d{1,2}\"],"\
+      "\"examples\": [\"00002814100828140000000003C00000 -> dead\"]"\
+    "},"\
+    "{"\
+      "\"patterns\": [\"^propsync\\\\.forcesyncfreq$\"],"\
+      "\"descriptions\": [\"Frequency at which the device performs a full synchronization with the server (required as the server performs regular cleanup on old records).\"],"\
+      "\"examples\": [\"~72h\"]"\
+    "},"\
+    "{"\
+      "\"patterns\": [\"^settings\\\\.lcdlogs$\"],"\
+      "\"descriptions\": [\"Flag telling if the LCD shows the logs or not\"],"\
+      "\"examples\": ["\
+      "\"true -> logs in LCD on\","\
+      "\"false -> logs in LCD off\""\
+      "]"\
+    "},"\
+    "{"\
+      "\"patterns\": [\"^settings\\\\.mperiodms$\"],"\
+      "\"descriptions\": [\"User response time in ms. The lower this value the more power consumption.\"],"\
+      "\"examples\": ["\
+        "\"1000 -> 1 second of response timelogs in LCD on\""\
+      "]"\
+    "},"\
+    "{"\
+      "\"patterns\": [\"^settings\\\\.periodms$\"],"\
+      "\"descriptions\": [\"Period (in ms) of wakeup.\"],"\
+      "\"examples\": ["\
+        "\"10000 -> every 10 seconds\","\
+        "\"60000 -> every 60 seconds\""\
+      "]"\
+    "},"\
+    "{"\
+      "\"patterns\": [\"^settings\\\\.btnrout$\"],"\
+      "\"descriptions\": [\"Determines which first moves will be executed randomly on button press. For instance, if set to 2, only moves 0 and 1 will be executed randomly. \"],"\
+      "\"examples\": ["\
+        "\"0\","\
+        "\"1\","\
+        "\"2\","\
+        "\"3\""\
+      "]"\
+    "},"\
+    "{"\
+      "\"patterns\": [\"^.*\\\\.debug\"],"\
+      "\"descriptions\": [\"Flag to enable/disable debug mode\"],"\
+      "\"examples\": ["\
+        "\"true -> debug enabled\","\
+        "\"false -> debug disabled\""\
+      "]"\
+    "}"\
+  "]"\
+"}"
+
 
 bool initWifiSimple() {
   Settings *s = m->getModuleSettings();
