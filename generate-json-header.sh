8b80a98c2d7b142d8ad75415fe54ca8fb2a5bc6d
Mauricio Jost <mauricio.jost@gmail.com>
2019-10-22 21:13:21 +0200

    Fix json header generation

--- generate-json-header.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "#define VERSION_DESCRIPTION_JSON \"" > src/Description.json.h
-
-cat src/Description.json | jq -c . | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' >> src/Description.json.h
-
-echo "\"" >> src/Description.json.h
-
-cat src/Description.json.h | tr --delete '\n'  > src/Description.json.2.h 
