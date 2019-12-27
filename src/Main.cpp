ebe4c4bcde93be3fe18ce11b63c232946b23b73c
Mauricio Jost <mauricio.jost@gmail.com>
2019-12-28 00:25:58 +0100

    Fix description pushing

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,5 @@
 #include <Main.h>
 
-#include <Description.json.h>
 
 void setup() {
   m = new ModuleBotino();
@@ -28,10 +27,9 @@ void setup() {
            getLogBuffer
            );
 
-  m->getModule()->setDescription(VERSION_DESCRIPTION_JSON);
-
   log(CLASS_MAIN, Info, "Startup of properties");
   ModuleStartupPropertiesCode ec = m->startupProperties();
+
 
   if (ec != ModuleStartupPropertiesCodeSuccess) {
     log(CLASS_MAIN, Error, "Failure: %d", (int)ec);
@@ -41,6 +39,16 @@ void setup() {
     } else {
       abort("Could not startup");
     }
+  } else {
+    // Tricky description pushing: unless declared specially (with PROGMEM for instance)
+    // constant variables are kept in RAM. This is a 4K object that cannot stay there.
+    // https://arduino-esp8266.readthedocs.io/en/latest/PROGMEM.html
+    // Solution: put it in PROGMEM and store it in RAM only when required to push.
+#include <Description.json.h>
+    String desc = String(DESCRIPTION_JSON_VERSION);
+    log(CLASS_MAIN, Debug, "Pushing description...");
+    logRaw(CLASS_MAIN, Debug, desc.c_str());
+    m->getModule()->getPropSync()->pushDescription(desc.c_str());
   }
 }
 
