6a3f990f02ce4d7162ee852469a5ef7ec4a40887
Mauricio Jost <mauriciojost@gmail.com>
2020-04-27 23:43:44 +0200

    Show answer to question

--- src/PlatformESP.h
+++ src/PlatformESP.h
@@ -17,8 +17,9 @@ void askStringQuestion(const char *question, Buffer *answer) {
   log(CLASS_PLATFORM, User, "Question: %s", question);
   Serial.setTimeout(QUESTION_ANSWER_TIMEOUT_MS);
   Serial.readBytesUntil('\n', answer->getUnsafeBuffer(), answer->getCapacity());
-  answer->replace('\n', '\0');
-  answer->replace('\r', '\0');
+  answer->replace('\n', 0);
+  answer->replace('\r', 0);
+  log(CLASS_PLATFORM, User, "Answer: '%s'", answer->getBuffer());
 }
 
 #endif // PLATFORM_ESP_INC
